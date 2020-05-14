c4c1edac1bb4e2c37bb572f0e5c1a0eec5035c90
Haydar <haydarpehlivan61@hotmail.com>
2020-05-14 16:21:06 +0200

    ✨ add login functionality

--- routes/api.php
+++ routes/api.php
@@ -7,23 +7,12 @@ use App\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('API')->group(function () {
+    Route::namespace('Auth')->group(function () {
+        Route::post('/login', 'LoginController')->name('login');
+    });
+
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
-
-Route::post('/login', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-
-    if (!$user || !Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
-
-    return $user->createToken($request->email)->plainTextToken;
-})->name('login');
