6f2fd23d4875aa3241c11d5829963192d5ecf26a
Matthijs Kropholler <matthijsk2000@pm.me>
2020-05-14 16:27:35 +0200

    🚧 add vue authentication

--- resources/frontend/app/src/main.ts
+++ resources/frontend/app/src/main.ts
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import store from './store/index'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:8000/'
+
+store.dispatch('auth/me').then(() => {
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+})
---
+++ resources/frontend/app/src/store/auth.ts
@@ -0,0 +1,52 @@
+import axios from 'axios'
+
+export default {
+  namespaced: true,
+
+  state: {
+    authenticated: false,
+    user: null
+  },
+
+  getters: {
+    authenticated(state: any) {
+      return state.authenticated
+    },
+    user(state: any) {
+      return state.user
+    }
+  },
+
+  mutations: {
+    SET_AUTHENTICATED(state: any, value: any) {
+      state.authenticated = value
+    },
+
+    SET_USER(state: any, value: any) {
+      state.user = value
+    }
+  },
+
+  actions: {
+    async signIn({dispatch}: any, credentials: any) {
+      await axios.get('/sanctum/csrf-cookie')
+      await axios.post('/login', credentials)
+
+      return dispatch('me')
+    },
+    async signOut({dispatch}: any) {
+      await axios.post('/logout')
+
+      return dispatch('me')
+    },
+    me({commit}: any) {
+      return axios.get('/api/user').then((response) => {
+        commit('SET_AUTHENTICATED', true)
+        commit('SET_USER', response.data)
+      }).catch(() => {
+        commit('SET_AUTHENTICATED', false)
+        commit('SET_USER', null)
+      })
+    }
+  }
+}
