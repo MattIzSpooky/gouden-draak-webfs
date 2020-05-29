a4d3f22fc25fab53b42845bf506328c7a9b48fb7
Haydar <haydarpehlivan61@hotmail.com>
2020-05-29 17:36:56 +0200

    ✨ make new users for testing

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use App\UserRole;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -12,6 +13,27 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class, 5)->create();
+        User::create([
+            'name' => 'Admin User',
+            'badge' => 10,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'user_role_id' => UserRole::ADMIN
+        ]);
+
+        User::create([
+            'name' => 'Waitress User',
+            'badge' => 15,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'user_role_id' => UserRole::WAITRESS
+        ]);
+
+        User::create([
+            'name' => 'Kassa User',
+            'badge' => 20,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'user_role_id' => UserRole::KASSA
+        ]);
+
+        factory(User::class, 3)->create();
     }
 }
