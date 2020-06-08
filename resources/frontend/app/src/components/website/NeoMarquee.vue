8273d87830cac1bdbc6cd499cb95821b1624f785
Matthijs Kropholler <matthijsk2000@pm.me>
2020-06-08 17:01:59 +0200

    ✨  add marquee

---
+++ resources/frontend/app/src/components/website/NeoMarquee.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="marquee">
+    <p>
+      {{text}}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from 'vue-property-decorator';
+
+  @Component
+export default class NeoMarquee extends Vue {
+    @Prop(String) public readonly text!: string;
+};
+</script>
+
+<style scoped lang="scss">
+  .marquee {
+    height: 50px;
+    overflow: hidden;
+    position: relative;
+    color: yellow;
+    font-weight: bold;
+    text-decoration: none;
+
+    p {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      margin: 0;
+      animation: scroll-left 20s linear infinite;
+    }
+
+    @keyframes scroll-left {
+      0% {
+        transform: translateX(100%);
+      }
+      100% {
+        transform: translateX(-100%);
+      }
+    }
+  }
+</style>
